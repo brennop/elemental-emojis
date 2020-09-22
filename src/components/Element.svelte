cf58926eae06adaca45a89473c900121e5b403d1
brennop <brennopcordeiro@gmail.com>
2020-09-22 11:12:08 -0300

    ⭐ add click to combine

--- src/components/Element.svelte
+++ src/components/Element.svelte
@@ -18,13 +18,25 @@
     size.set(1);
   });
 
-  function handleDrop(event) {
-    event.preventDefault();
-
-    const recipe = getRecipe([$source, value]);
+  function combineElements(source, target) {
+    const recipe = getRecipe([source, target]);
 
     if (recipe) {
       progress.update(($elements) => $elements.add(recipe.output));
+    }
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    combineElements($source, value);
+  }
+
+  function handleClick() {
+    if ($source == null) {
+      source.set(value);
+    } else {
+      combineElements($source, value);
+      source.set();
     }
   }
 </script>
@@ -52,12 +64,13 @@
     display: flex;
     align-items: center;
     flex-direction: column;
-    transition: 0.1s ease-out;
+    transition: 0.2s ease-out;
     justify-content: space-between;
+    box-shadow: inset 0 0 0px 0px #2fd3fc20;
   }
 
   .container:hover {
-    background-color: #f0f0f0;
+    box-shadow: inset 0 0 4px 4px #2fd3fc20;
   }
 
   .hover {
@@ -68,14 +81,24 @@
     font-size: 0.8rem;
     text-align: center;
   }
+
+  .selected {
+    box-shadow: inset 0 0 2px 6px #2fd3fc40;
+  }
+
+  .selected:hover {
+    box-shadow: inset 0 0 2px 6px #2fd3fc40;
+  }
 </style>
 
 <div
+  class:selected={$source === value}
   class="container"
   class:hover={isHovered}
   use:draggable={value}
   use:hoverable={(value) => (isHovered = value)}
-  on:drop={handleDrop}>
+  on:drop={handleDrop}
+  on:click={handleClick}>
   <div class="item" style="transform: scale({$size})">{emoji}</div>
   <span>{displayName}</span>
 </div>
