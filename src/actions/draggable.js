951f801dfd6c2648443e6610d34ec93ee7b08b7a
brennop <brennopcordeiro@gmail.com>
2020-09-23 22:34:32 -0300

    🐭 improves drag and drop wow

--- src/actions/draggable.js
+++ src/actions/draggable.js
@@ -1,19 +1,43 @@
 import { source, dragging } from "../store";
 
 export function draggable(node, value) {
-  let x;
-  let y;
-  let timer;
+  let x, dx;
+  let y, dy;
+  let timers = [];
 
-  const handleMouseDown = () => {
-    timer = setTimeout(() => {
+  const handleMouseDown = (event) => {
+    x = event.clientX;
+    y = event.clientY;
+
+    timers = [
+      setTimeout(() => {
+        window.addEventListener("mousemove", handleMouseMove);
+      }, 50),
+      setTimeout(() => {
+        source.set(value);
+        dragging.set(true);
+      }, 500),
+    ];
+  };
+
+  // TODO: create a writable store to share this event
+  // with Float.svelte since this is bound everytime
+  const handleMouseMove = (event) => {
+    dx = event.clientX - x;
+    dy = event.clientY - y;
+
+    window.removeEventListener("mousemove", handleMouseMove);
+
+    // these values are completely arbitrary
+    if (dx > Math.abs(10) || dy > Math.abs(10)) {
+      timers.forEach((timer) => clearTimeout(timer));
       source.set(value);
       dragging.set(true);
-    }, 100);
+    }
   };
 
   const handleMouseUp = () => {
-    clearTimeout(timer);
+    timers.forEach((timer) => clearTimeout(timer));
   };
 
   node.addEventListener("mousedown", handleMouseDown);
@@ -21,8 +45,9 @@ export function draggable(node, value) {
 
   return {
     destroy() {
-      node.removeEventListener(handleMouseDown);
-      node.removeEventListener(handleMouseUp);
+      node.removeEventListener("mousemove", handleMouseDown);
+      node.removeEventListener("mouseup", handleMouseUp);
+      window.removeEventListener("mousemove", handleMouseMove);
     },
   };
 }
