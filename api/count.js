c25a412ea702213c46d20e7ed0fefd2e61c7c2d0
brennop <brennopcordeiro@gmail.com>
2020-09-25 16:23:53 -0300

    📈 fix analytics (again)

--- api/count.js
+++ api/count.js
@@ -1,3 +1,14 @@
+import axios from "axios";
+
 module.exports = (req, res) => {
-  res.redirect("https://elementalemojis.goatcounter.com/count");
+  const { headers, params } = req;
+
+  const url = "https://elementalemojis.goatcounter.com/count";
+
+  console.log("Couting view");
+
+  axios
+    .get(url, { headers, params })
+    .then((response) => res.send(response.data))
+    .catch(console.error);
 };
