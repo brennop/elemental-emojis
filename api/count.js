abce5f628c02d0c189944f42e9a9608db2c69e11
brennop <brennopcordeiro@gmail.com>
2020-09-26 21:00:21 -0300

    Revert "📈 fix analytics (again)"
    This reverts commit c25a412ea702213c46d20e7ed0fefd2e61c7c2d0.

--- api/count.js
+++ api/count.js
@@ -1,14 +1,3 @@
-import axios from "axios";
-
 module.exports = (req, res) => {
-  const { headers, params } = req;
-
-  const url = "https://elementalemojis.goatcounter.com/count";
-
-  console.log("Couting view");
-
-  axios
-    .get(url, { headers, params })
-    .then((response) => res.send(response.data))
-    .catch(console.error);
+  res.redirect("https://elementalemojis.goatcounter.com/count");
 };
